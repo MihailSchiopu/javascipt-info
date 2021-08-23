16f28c8b668245896263296ba43a36e035d53bcd
Schiopu <mschiopu@techteam.com>
2021-08-23 21:23:56 +0300

    m

--- alarm/alarm.js
+++ alarm/alarm.js
@@ -12,43 +12,52 @@ let workingPendulum
 
 startBtn.addEventListener('click', () => {
     workingClock = setInterval(alarm, 1000)
-    setRotation()
+
 })
 
 stopBtn.addEventListener('click', () => {
     clearInterval(workingClock)
-    clearInterval(workingPendulum)
+
 })
 
-let setRotation = async () => {
+let setRotation = () => {
 
-    workingPendulum = await setInterval(() => {
-        pendulum.style.setProperty("--rotation", positions[step])
-        step++
+    pendulum.style.setProperty("--rotation", positions[step])
+    step++
 
-        if (step >= 2) {
-            positions.reverse()
-            step = 0
+    if (step >= 2) {
+        positions.reverse()
+        step = 0
 
-        }
-    }, 1000)
+    }
 }
 
 const alarm = () => {
-    let day = new Date()
-    let hr = day.getHours() < 10 ? '0' + day.getHours() : day.getHours()
-    let min = day.getMinutes() < 10 ? '0' + day.getMinutes() : day.getMinutes()
-    let sec = day.getSeconds() < 10 ? '0' + day.getSeconds() : day.getSeconds()
-    clock.innerHTML = `<p class="fs-3 mt-3">${hr} : ${min} : ${sec} </p>`
+    new Promise((resolve) => {
+        let day = new Date()
+        let hr = day.getHours() < 10 ? '0' + day.getHours() : day.getHours()
+        let min = day.getMinutes() < 10 ? '0' + day.getMinutes() : day.getMinutes()
+        let sec = day.getSeconds() < 10 ? '0' + day.getSeconds() : day.getSeconds()
 
-    if (sec == 00) {
-        try {
-            ll
-        } catch (error) {
+        clock.innerHTML = `<p class="fs-3 mt-3">${hr} : ${min} : ${sec} </p>`
+
+        if (sec == 00) {
+            throw new Error
+        }
+
+        resolve()
+
+    })
+        .then(resolve => {
+            setRotation()
+            setTimeout(setRotation, 1000)
+
+        })
+        .catch(() => {
             alarmMessage.innerHTML = `<p class="fs-3 mt-3" id="mess">It's perfect time to wake up !!! </p>`
             setTimeout(() => document.getElementById("mess").remove(), 5000)
-        }
-    }
+        })
+
 }
 
 
